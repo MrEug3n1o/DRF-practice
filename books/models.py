51302d6481efae535ed4ab27e6df14fe317f3dfe
mr_eugene <roberttimethone@gmail.com>
2025-12-19 21:57:07 +0100

    models for books created

--- books/models.py
+++ books/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    title = models.CharField(max_length=255, unique=True)
+    author = models.CharField(max_length=255)
+
+    class CoverType(models.TextChoices):
+        HARD = ("HARD", )
+        SOFT = ("SOFT", )
+
+    cover = models.CharField(
+        max_length=4,
+        choices=CoverType,
+        default=CoverType.HARD
+    )
+    inventory = models.PositiveIntegerField()
+    daily_fee = models.DecimalField(max_digits=6, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.title} by {self.author}"
+
+    class Meta:
+        ordering = ["title"]
