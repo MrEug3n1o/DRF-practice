16ac827fdc2df35a82c4be59afc45552b9ae3243
mr_eugene <roberttimethone@gmail.com>
2025-12-22 21:46:48 +0100

    added urls and admin books

--- library/settings.py
+++ library/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'rest_framework',
 
     'books',
+    'user',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +119,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+AUTH_USER_MODEL = 'user.User'
