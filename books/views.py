108bdcc2904539a5d5b171b1ea0d25643523a67c
mr_eugene <roberttimethone@gmail.com>
2025-12-24 18:44:45 +0100

    added get_permission method in views in books app

--- books/views.py
+++ books/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
 from rest_framework import viewsets
+from rest_framework.permissions import IsAdminUser, AllowAny
 
 from .models import Book
 from .serializers import BookSerializer
@@ -8,3 +8,8 @@ from .serializers import BookSerializer
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+
+    def get_permissions(self):
+        if self.action in ["list", "retrieve"]:
+            return [AllowAny()]
+        return [IsAdminUser()]
