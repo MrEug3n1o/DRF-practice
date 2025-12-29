bc78e5795d88970abcd980b891f9ca3e03d3a5b7
mr_eugene <roberttimethone@gmail.com>
2025-12-29 21:39:33 +0100

    added serializer for detailed book info and implemented it to views

--- borrowing/views.py
+++ borrowing/views.py
@@ -7,9 +7,15 @@ from borrowing.models import Borrowing
 
 from borrowing.serializers import (
     BorrowingSerializer,
+    BorrowingBookDetailSerializer,
 )
 
 
 class BorrowingViewSet(viewsets.ModelViewSet):
     queryset = Borrowing.objects.all()
     serializer_class = BorrowingSerializer
+
+    def get_serializer_class(self):
+        if self.action == "retrieve":
+            return BorrowingBookDetailSerializer
+        return BorrowingSerializer
